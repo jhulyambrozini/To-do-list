907c3df3e434097211c3f04cb3b5cd6506f947ec
jhulyamfebr@gmail.com <jhulyambrozini@gmail.com>
2023-04-25 18:41:38 -0300

    docs: atualizando README.md

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # To-do-list
+
+Esse projeto foi feito com o objetivo de práticar meus conhecimentos de React.js, ou seja, sem fins lucrativos. Também foi feito um simulador de API para os dados das tarefas, a fim de tornar mais fácil manipulação dos mesmos.
+
+# Tecnologias utilizadas
+- React.js
+
+# Execução
+- baixar ou clonar este repositório;
+- executar os comandos: `npm install` e `npm start`, respectivamente para startar o projeto;
+- executar o comando `npm run server`, para iniciar uma simulação de API.
+
+# Layout
+![layout](./src/assets/layout.png)
--- src/App.js
+++ src/App.js
@@ -11,21 +11,77 @@ function App() {
   const [todos, setTodos] = useState([])
   const [loading, setLoading] = useState(false)
 
-  const handleSubmit = e => {
+  useEffect(() => {
+    // requisiçãode dados da API
+    const loadData = async () => {
+      setLoading(true)
+
+      const res = await fetch(API + "/todos")
+        .then((res) => res.json())
+        .then((data) => data)
+        .catch((err) => console.log(err))
+
+      setLoading(false)
+      setTodos(res)
+    }
+
+    loadData()
+  }, [])
+
+  // obj que vai ser enviado para API
+  const handleSubmit = async e => {
     e.preventDefault()
-   const todo = {
-    id: Math.random(),
-    title,
-    time,
-    done: false
-   }
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done: false
+    }
 
-   console.log(todo)
+    // enviando dados para api
+    await fetch(API + "/todos", {
+      method: "POST",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
 
-   setTime('')
-   setTitle('')
+    setTodos((prevState) => [...prevState, todo])
+
+    setTime('')
+    setTitle('')
   }
 
+  const handleDelete = async id => {
+
+    await fetch(API + "/todos/" + id, {
+      method: "DELETE",
+    })
+
+    setTodos(prevState => prevState.filter(todo => todo.id !== id))
+
+  }
+
+  const handleCheck = async todo => {
+
+    todo.done = !todo.done
+
+    const data = await fetch(API + "/todos/" + todo.id, {
+      method: "PUT",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+
+    setTodos(prevState => prevState.map(t => (t.id === data.id ? (t = data) : t)))
+
+  }
+
+  if (loading) {
+    return <p>Carregando...</p>
+  }
 
   return (
     <div className="App">
@@ -38,23 +94,23 @@ function App() {
           <div className='form-control'>
             <label htmlFor='title'>O que você vai fazer?</label>
             <input
-            type='text'
-            name='title'
-            placeholder='Título da tarefa'
-            onChange={e => setTitle(e.target.value)}
-            value={title}
-            required
+              type='text'
+              name='title'
+              placeholder='Título da tarefa'
+              onChange={e => setTitle(e.target.value)}
+              value={title}
+              required
             />
           </div>
           <div className='form-control'>
             <label htmlFor='time'>Duração:</label>
             <input
-            type='text'
-            name='time'
-            placeholder='Tempo estimado(em horas)'
-            onChange={e => setTime(e.target.value)}
-            value={time}
-            required
+              type='text'
+              name='time'
+              placeholder='Tempo estimado (em horas)'
+              onChange={e => setTime(e.target.value)}
+              value={time}
+              required
             />
           </div>
           <button type='submit'>Criar tarefa</button>
@@ -62,7 +118,22 @@ function App() {
       </div>
       <div className='list-todo'>
         <h2>Lista de tarefas:</h2>
+        {/* curto circuito para verificar se tem tarefa */}
         {todos.length === 0 && <p>Não há tarefas!</p>}
+
+        {/* inserção das tarefas */}
+        {todos.map((todo) => (
+          <div className='todo' key={todo.id}>
+            <h3 className={todo.done ? 'todo-done' : ''}>{todo.title}</h3>
+            <p>Duração: {todo.time}h</p>
+            <div className='actions'>
+              <span onClick={() => handleCheck(todo)}>
+                {!todo.done ? <BsBookmarkCheck /> : <BsBookmarkFill />}
+              </span>
+              <BsTrash onClick={() => handleDelete(todo.id)} />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
